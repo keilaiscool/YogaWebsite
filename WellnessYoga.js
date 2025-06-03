200de76e46d7fc6eea7b70a0b26ad764e1889619
keilaiscool <keilaw712@gmail.com>
2025-06-03 11:08:09 -0700

    Add files via upload

---
+++ WellnessYoga.js
@@ -0,0 +1,9 @@
+
+  function displaySentence() {
+    
+    document.getElementById('formc').innerHTML = "Thank you!";
+  }
+ 
+function displayReceipt() {
+    document.getElementById('purchase').innerHTML = "Thank you for your purchase. We look forward to seeing you soon!";
+}
